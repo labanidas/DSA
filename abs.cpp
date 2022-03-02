aa9bc53ecbee2300ee42ac27ce10932b4e6b7251
labanidas <88694887+labanidas@users.noreply.github.com>
2022-03-02 16:47:50 +0530

    c++

---
+++ abs.cpp
@@ -0,0 +1,9 @@
+/*The abs() function in C++ returns the absolute value of an integer number. This function is defined in the cstdlib header file.*/
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	cout<<abs(-2)<<endl;
+}

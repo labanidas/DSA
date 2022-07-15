0c118e707c2d49e07133103b61de18f2fdf1b399
Labani Das <88694887+labanidas@users.noreply.github.com>
2022-07-15 19:36:03 +0530

    radix sort

--- radix-sort.cpp
+++ radix-sort.cpp
@@ -7,28 +7,28 @@ countSort(int arr[], int n, int pass)
 	
 	int b[n];
 	
-	for(int i=0;i<n;i++) count[(arr[i]/pass)%10]++;
-//	for(int i=1;i<10;i++) cout<<count[i]<<endl;
-	
-	for(int i=1;i<10;i++) count[i]=count[i-1]+count[i];
-//	for(int i=1;i<10;i++) cout<<count[i]<<endl;
-	
-	for(int i=n-1;i>=0;i--) {
-		b[count[(arr[i]/pass)*10]-1] = arr[i];
-		count[(arr[i]/pass)*10]--;
-	}
-	for(int i=0;i<n;i++) arr[i] = b[i];
-//	copy(b ,b+n, arr);
+	for(int i=0;i<n;i++)	count[(arr[i]/pass)%10]++;
 
 	
+	for(int i=1;i<10;i++) count[i]=count[i-1]+count[i];
+
+	
+	for(int i=n-1;i>=0;i--) {
+		b[count[(arr[i]/pass)%10]-1] = arr[i];
+		count[(arr[i]/pass)%10]--;
+	}
+	
+	copy(b ,b+n, arr);
+	
 }
+
 radixSort(int arr[], int n)
 {
 	int max =  *max_element(arr,arr+n);
 	
-//	cout<<"max "<<max<<endl;
+
 	for(int pass=1; max/pass>0; pass*=10) 	{
-//		cout<<"pass "<<pass<<endl;
+
 		countSort(arr,n,pass);
 	}
 		

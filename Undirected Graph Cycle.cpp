4e05c01c7108089352711341eea5d4f82778a5f0
Labani Das <88694887+labanidas@users.noreply.github.com>
2024-07-28 07:11:27 -0700

    Update Undirected Graph Cycle.cpp

--- Undirected Graph Cycle.cpp
+++ Undirected Graph Cycle.cpp
@@ -1,5 +1,7 @@
 //https://www.geeksforgeeks.org/problems/detect-cycle-in-an-undirected-graph/1
 
+
+// ------------------------------------------------ DFS ---------------------------------------------------
 class Solution {
   public:
   
@@ -19,6 +21,47 @@ class Solution {
         vector<int> visited(V, false);
         for(int i=0 ; i<V; i++){
             if(!visited[i] && DFS(adj, i, visited, -1))
+                return true;
+        }
+        return false;
+    }
+};
+
+
+// ------------------------------------------------ BFS ---------------------------------------------------
+class Solution {
+  public:
+  
+    bool BFS(auto& adj, int u, auto &visited){
+        queue<pair<int, int>> q;
+        q.push({u, -1});
+        visited[u] = true;
+        
+        while(!q.empty()){
+            pair<int, int>  P = q.front(); 
+            q.pop();
+            int source = P.first;
+            int parent = P.second;
+            
+           
+            
+            for(auto &v : adj[source]){
+                if(!visited[v]){
+                    q.push({v, source});
+                    visited[v] = true;
+                }else if(v != parent){
+                    return true;
+                }
+            }
+        }
+        
+        return false;
+    }
+    // Function to detect cycle in an undirected graph.
+    bool isCycle(int V, vector<int> adj[]) {
+        vector<bool> visited(V, false);
+        for(int i=0 ; i<V; i++){
+            if(!visited[i] && BFS(adj, i, visited))
                 return true;
         }
         return false;

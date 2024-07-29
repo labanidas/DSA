8d240bee92778e37a1b02989be1ac7f45a6939a3
Labani Das <88694887+labanidas@users.noreply.github.com>
2024-07-28 22:46:00 -0700

    Update Topological sort.cpp

--- Topological sort.cpp
+++ Topological sort.cpp
@@ -1,11 +1,10 @@
-//Topological Sort using DFS -- https://www.youtube.com/watch?v=WbbYZRr4arw&list=PLpIkg8OmuX-LZB9jYzbbZchk277H5CbdY&index=7
 // https://www.geeksforgeeks.org/problems/topological-sort/1
 
 
 // ---------------------------------------- DFS ---------------------------------------------
 // ---------------------------------------- DFS ---------------------------------------------
 // ---------------------------------------- DFS ---------------------------------------------
-
+//Topological Sort using DFS -- https://www.youtube.com/watch?v=WbbYZRr4arw&list=PLpIkg8OmuX-LZB9jYzbbZchk277H5CbdY&index=7
 class Solution
 {
 	public:
@@ -45,4 +44,47 @@ class Solution
 // ---------------------------------------- BFS ---------------------------------------------
 // ---------------------------------------- BFS ---------------------------------------------
 // ---------------------------------------- BFS ---------------------------------------------
+// Kahn's Algorithm(Topological Sort using BFS) -- https://www.youtube.com/watch?v=uVl4ftleTes&list=PLpIkg8OmuX-LZB9jYzbbZchk277H5CbdY&index=8
+class Solution
+{
+	public:
+	void topoSortBFS(auto adj, auto& indegree, auto& result, int V){
+	    
+	    queue<int> q;
+	    
+	    for(int i =0; i<V; i++){
+    	     if(indegree[i] == 0)
+    	        q.push(i);
+    	 }
+        while(!q.empty()){
+            
+            int u = q.front();
+            q.pop();
+            result.push_back(u);
+            
+            for(auto& v: adj[u]){
+                indegree[v]--;
+                if(indegree[v] == 0)
+                    q.push(v);
+            }
+            
+        }
+    	 
+	}
+	//Function to return list containing vertices in Topological order. 
+	vector<int> topoSort(int V, vector<int> adj[]) 
+	{   
+	    vector<int> indegree(V, 0);
+	    vector<int> result;
+	    
+    	 for(int i=0; i<V; i++){
+    	     for(auto& u: adj[i]){
+    	         indegree[u]++;
+    	     }
+    	 }
+    	 
+    	 topoSortBFS(adj, indegree, result, V);
+    	 return result;
+	}
+};
 
